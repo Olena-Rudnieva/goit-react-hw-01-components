ebfca3385b3fdb9b31a4ae1265a79a685b822a56
Olena-Rudnieva <rudnievao@gmail.com>
2023-07-10 15:59:20 +0300

    Add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { profile } from './Profile/Profile';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return profile;
+  // <div
+  //   style={{
+  //     height: '100vh',
+  //     display: 'flex',
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //     fontSize: 40,
+  //     color: '#010101',
+  //   }}
+  // >
+  //   React homework template
+  // </div>
 };
