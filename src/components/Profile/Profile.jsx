ecbf3bd6b765eabacde96f6b2374a5cf839ea001
Olena-Rudnieva <rudnievao@gmail.com>
2023-07-11 15:40:10 +0300

    Update styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,28 @@
 import user from '../../../src/user.json';
+import PropTypes from 'prop-types';
+import css from './Profile.module.css';
 
 export const Profile = (
-  <div className="profile">
-    <div className="description">
-      <img src={user.avatar} alt={user.username} className="avatar" />
-      <p className="name">{user.username}</p>
-      <p className="tag">@{user.tag}</p>
-      <p className="location">{user.location}</p>
+  <div className={css.profile}>
+    <div className={css.description}>
+      <img src={user.avatar} alt={user.username} className={css.avatar} />
+      <p className={css.name}>{user.username}</p>
+      <p className={css.tag}>@{user.tag}</p>
+      <p className={css.location}>{user.location}</p>
     </div>
 
-    <ul className="stats">
+    <ul className={css.stats}>
       <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{user.stats.followers}</span>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{user.stats.followers}</span>
       </li>
       <li>
-        <span className="label">Views</span>
-        <span className="quantity">{user.stats.views}</span>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{user.stats.views}</span>
       </li>
       <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{user.stats.likes}</span>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{user.stats.likes}</span>
       </li>
     </ul>
   </div>
