3cd6b449c2b11b3491e8a30708b4748e3b3d9fbc
Olena-Rudnieva <rudnievao@gmail.com>
2023-07-13 17:40:43 +0300

    Add friendslist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import user from '../../src/user.json';
 import data from '../../src/data.json';
+import friends from '../../src/friends.json';
 import { Profile } from './Profile/Profile';
 import { StatisticList } from './Statistics/StatisticList';
+import { FriendList } from './FriendList/FriendList';
 
 export const App = () => {
   return (
@@ -14,6 +16,7 @@ export const App = () => {
         stats={user.stats}
       />
       <StatisticList title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
